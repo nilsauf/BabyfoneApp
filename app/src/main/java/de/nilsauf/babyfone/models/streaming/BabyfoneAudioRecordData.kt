734900b9a1a673c1f183c6ce185946dd257fbe1e
Nils Aufschläger <mail@nilsauf.de>
2023-01-18 21:28:24 +0100

    make BabyfoneAudioRecordData a data class

--- app/src/main/java/de/nilsauf/babyfone/models/streaming/BabyfoneAudioRecordData.kt
+++ app/src/main/java/de/nilsauf/babyfone/models/streaming/BabyfoneAudioRecordData.kt
@@ -1,3 +1,21 @@
 package de.nilsauf.babyfone.models.streaming
 
-class BabyfoneAudioRecordData(val Data: ByteArray, val Length: Int)
+data class BabyfoneAudioRecordData(val Data: ByteArray, val Length: Int) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BabyfoneAudioRecordData
+
+        if (!Data.contentEquals(other.Data)) return false
+        if (Length != other.Length) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = Data.contentHashCode()
+        result = 31 * result + Length
+        return result
+    }
+}
